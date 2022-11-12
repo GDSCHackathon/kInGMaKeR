4b7df97ef37482361722067431ff971dae991d8f
dohun <dohun31@naver.com>
2022-11-13 00:33:31 +0900

    feat: 뒤로가기 방지

--- frontend/src/page/error/Error.js
+++ frontend/src/page/error/Error.js
@@ -3,7 +3,7 @@ import "./Error.scss";
 
 const Error = () => {
   return (
-    <div>
+    <div className="error-container">
       <p id="errorMessage">
         찾을 수 없는 페이지입니다. <br />
         요청하신 페이지가 사라졌거나, 잘못된 경로를 이용하셨어요. :P
