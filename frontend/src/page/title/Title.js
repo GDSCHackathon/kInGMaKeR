6bf4cb3f8f8a7a86988242126ae4aed2735d5868
zzuzzu0205~ <zkakcnd@naver.com>
2022-11-13 01:52:08 +0900

    font

--- frontend/src/page/title/Title.js
+++ frontend/src/page/title/Title.js
@@ -5,22 +5,52 @@ import { Link } from "react-router-dom";
 function Title() {
   return (
     <div>
-      {/* <span id="heesu">JUNG HEE SU</span>
-      <span id="jinsu">KIM JIN SU</span>
-      <span id="byungju">PARK BYUNG JU</span>
-      <span id="dohoon">BAEK DO HOON</span> */}
       <div>
         <Link to="/error">
-          <button id="startButton">S</button>
+          <button id="startButton">{">"}</button>
         </Link>
       </div>
-      <p id="title">kInGMaKeR</p>
+      <p id="title">
+        <span>k</span>
+        <span>I</span>
+        <span>n</span>
+        <span>G</span>
+        <span>M</span>
+        <span>a</span>
+        <span>K</span>
+        <span>e</span>
+        <span>R</span>
+      </p>
+      <div class="imgHoverEvent event1">
+        <div class="imgBox"></div>
+        <div class="hoverBox">
+          <span class="text">BAEK DO HOON</span>
+        </div>
+      </div>
+      <div class="imgHoverEvent event1">
+        <div class="imgBox"></div>
+        <div class="hoverBox">
+          <span class="text">PARK BYUNG JU</span>
+        </div>
+      </div>
       <div class="imgHoverEvent event1">
         <div class="imgBox"></div>
         <div class="hoverBox">
           <Link to="/qna">시작하기</Link>
         </div>{" "}
         <Link to="/qna"></Link>
+      </div>
+      <div class="imgHoverEvent event1">
+        <div class="imgBox"></div>
+        <div class="hoverBox">
+          <span class="text">KIM JIN SU</span>
+        </div>
+      </div>
+      <div class="imgHoverEvent event1">
+        <div class="imgBox"></div>
+        <div class="hoverBox">
+          <span class="text">JUNG HEE SU</span>
+        </div>
       </div>
     </div>
   );
