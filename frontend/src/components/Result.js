7e9902a54a740d875221301377daff59bdca0229
dohun <dohun31@naver.com>
2022-11-13 00:48:30 +0900

    fix: ESLint 에러 수정

--- frontend/src/components/Result.js
+++ frontend/src/components/Result.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import useConfetti from "../utils/useConfetti";
 
 function Result() {
