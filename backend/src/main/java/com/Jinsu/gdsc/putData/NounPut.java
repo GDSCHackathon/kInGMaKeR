6c3bc2abeb24dc3358e3fb6744fce09b6c7c0b99
dohun <dohun31@naver.com>
2022-11-13 03:37:14 +0900

    refactor: 사용하지 않는 코드 제거

--- backend/src/main/java/com/Jinsu/gdsc/putData/NounPut.java
+++
@@ -1,4 +0,0 @@
-package com.Jinsu.gdsc.putData;
-
-public class NounPut {
-}
--- backend/src/main/java/com/Jinsu/gdsc/service/ResultService.java
+++ backend/src/main/java/com/Jinsu/gdsc/service/ResultService.java
@@ -1,42 +1,16 @@
 package com.Jinsu.gdsc.service;
-
-
-import com.Jinsu.gdsc.entitiy.Adjective;
-import com.Jinsu.gdsc.entitiy.Noun;
-import com.Jinsu.gdsc.repository.ARepository;
-import com.Jinsu.gdsc.repository.NRepository;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.List;
 
 @Service
 public class ResultService {
-    private final ARepository aRepository;
-    private final NRepository nRepository;
 
-    @Autowired
-    public ResultService(ARepository aRepository, NRepository nRepository) {
-        this.aRepository = aRepository;
-        this.nRepository = nRepository;
-    }
-
-    public List<Adjective> getRandomAdjective() {
-        List<Adjective> list = aRepository.findAll();
-
-        return list;
-    }
-
-    public List<Noun> getRandomNoun() {
-        List<Noun> list = nRepository.findAll();
-        return list;
-    }
 
     public String getResult() throws JSONException, IOException {
         URL url = null;
